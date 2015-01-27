55f0a59dbba4a94a56c8de29e1c179f8b2351447
Greg Levine <glevine@sugarcrm.com>
2015-01-27 14:33:08 -0500

    `Apps` represent deployed instances of the SugarCRM application with references to each of the components that make up its stacks. Tasks are an ordered list of steps that should be completed in the background.
    `Apps` are managed using a TingoDB database, whose files are stored in the db/ folder. Creating, fetching, removing, and removing all apps are supported actions through the REST API.

--- server.js
+++ server.js
@@ -14,20 +14,114 @@ server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
 // only allow application/json
-server.pre(function (req, res, next) {
+server.pre(function(req, res, next) {
     if (!req.is('json')) {
-        return next(new restify.NotAcceptableError('application/json is the only acceptable request type'));
+        next(new restify.NotAcceptableError('application/json is the only acceptable request type'));
+    } else {
+        next();
+    }
+});
+
+// database
+var mongoose, tungus;
+
+tungus = require('tungus');
+mongoose = require('mongoose');
+mongoose.connect('tingodb:///' + __dirname + '/db');
+
+// models
+var AppModel = mongoose.model('Apps', mongoose.Schema({
+    containers: [String],
+    source: String, // location of the source code
+    stack: {
+        database: {
+            name: String, // apache, oracle, db2
+            version: String
+        },
+        operating_system: {
+            name: String, // centos, ubuntu, windows
+            version: String
+        },
+        php: String, // version number
+        server: {
+            name: String, // apache, nginx
+            version: String
+        }
+    },
+    tasks: [String]
+}));
+
+// routes
+server.get('/apps/', function(req, res, next) {
+    function error(err) {
+        next(err);
     }
 
-    return next();
+    function success(data) {
+        res.send(data);
+        next();
+    }
+
+    AppModel.find({}).exec().then(success, error);
 });
 
-server.post('/apps/', function (req, res, next) {
-    res.send(req.params);
-    return next();
+server.post('/apps/', function(req, res, next) {
+    function error(err) {
+        next(err);
+    }
+
+    function success(data) {
+        res.send(data);
+        next();
+    }
+
+    AppModel.create(req.params).then(success, error);
 });
 
-server.listen(3000, function () {
+server.del('/apps/', function(req, res, next) {
+    function error(err) {
+        next(err);
+    }
+
+    function success(data) {
+        res.send(204);
+        next();
+    }
+
+    AppModel.remove().exec().then(success, error);
+});
+
+server.get('/apps/:id', function(req, res, next) {
+    function error(err) {
+        next(err);
+    }
+
+    function success(data) {
+        if (!data) {
+            next(new restify.ResourceNotFoundError('Could not find app ' + req.params.id));
+        } else {
+            res.send(data);
+            next();
+        }
+    }
+
+    AppModel.findById(req.params.id).exec().then(success, error);
+});
+
+server.del('/apps/:id', function(req, res, next) {
+    function error(err) {
+        next(err);
+    }
+
+    function success(data) {
+        res.send(204);
+        next();
+    }
+
+    AppModel.remove({_id: req.params.id}).exec().then(success, error);
+});
+
+server.listen(3000, function() {
     console.log('%s listening at %s', server.name, server.url);
 });
 
