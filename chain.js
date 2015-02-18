a9daebda8b3c3e082fcb977f4d51a6c30d8f3c92
Greg Levine <glevine@sugarcrm.com>
2015-02-18 13:10:19 -0500

    Plugins should be promises.

--- chain.js
+++ chain.js
@@ -1,5 +1,62 @@
 #!/usr/bin/env node
 
+var Q = require('q');
+
+function chain(names, funcs, app) {
+    var next;
+
+    function updateStatus(app, status) {
+        var deferred = Q.defer();
+
+        app.set({status: status});
+        app.save(function(err) {
+            if (err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(app);
+            }
+        });
+
+        return deferred.promise;
+    }
+
+    // var result = Q(initialValue)
+    // initialValue should be a function that shifts the next status off names and resolves with app, next
+
+    // in a while loop, result.then(updateStatus).then(callFunction)
+    // callFunction should be a function that shifts the next status off names and next func of funcs and calls func
+    // the task (func) will take app and return a promise
+    // when the task promise resolves, then we can resolve here to call the next updateStatus
+
+    // this enables next to be updated at the right moment in time
+    //TODO: trace through this logic
+
+    var result = Q(function() {
+        var deferred, first;
+
+        deferred = Q.defer();
+        first = names.shift();
+        deferred.resolve(app, first);
+
+        return deferred.promise;
+    });
+
+    while (names.length > 0) {
+        result.then(updateStatus).then(function(app) {
+            var deferred, func;
+
+            deferred = Q.defer();
+            func = funcs.shift();
+            next = names.shift();
+
+            var promise = func(app);
+            Q.when(promise, resolveWithAppAndNext, rejectWithError);
+
+            return deferred.promise;
+        });
+    }
+}
+
 var EventEmitter, keys, util;
 
 EventEmitter = require('events').EventEmitter;
